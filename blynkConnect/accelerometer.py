9c60deef548222514686077eb2fc13e511912fae
viduraDS <vidurads@gmail.com>
2024-09-06 18:04:49 +1000

    lisa added descriptions to accelerometer map

--- blynkConnect/accelerometer.py
+++ blynkConnect/accelerometer.py
@@ -3,39 +3,39 @@ import time
 
 class ADXL345:
     # ADXL345 Registers
-    THRESH_TAP = 0X1D
-    OFSX = 0X1E
-    OFSY = 0X1F
-    OFSZ = 0X20
-    DUR = 0X21
-    Latent = 0x22
-    Window = 0x23
-    THRESH_ACT = 0X24
-    THRESH_INACT = 0X25
-    TIME_INACT = 0X26
-    ACT_INACT_CTL = = 0X27
+    THRESH_TAP = 0X1D #Tap threshold
+    OFSX = 0X1E #X-axis offset
+    OFSY = 0X1F #Y-axis offset
+    OFSZ = 0X20 #Z-axis offset
+    DUR = 0X21 #Tap duration
+    Latent = 0x22 #Tap latency
+    Window = 0x23 #Tap window
+    THRESH_ACT = 0X24 #Activity threshold
+    THRESH_INACT = 0X25 #Inactivity threshold
+    TIME_INACT = 0X26 #Inactivity time
+    ACT_INACT_CTL = = 0X27 #Axis enable control for activity and inactivity detection
     
-    THRESH_FF = 0X28
+    THRESH_FF = 0X28 #Free-fall threshold
 
-    TIME_FF = 0X29
+    TIME_FF = 0X29 #Free-fall time
 
-    TAP_AXES = 0X2A
-    ACT_TAP_STATUS = 0X2B
-    BW_RATE = 0X2C
-    POWER_CTL = 0X2D
-    INT_ENABLE = 0X2E
-    INT_MAP = 0X2F
-    INT_SOURCE = 0X30 
-    DATA_FORMAT = 0x31
-    DATAX0 = 0x32
-    DATAX1 = 0X33
-    DATAY0 = 0X34
-    DATAY1 = 0X35
-    DATAZ0 = 0X36
-    DATAZ1 = 0X37
-    FIFO_CTL = 0X38
-    FIFO_STATUS = 0X39
-    
+    TAP_AXES = 0X2A #Axis control for single tap/double tap
+    ACT_TAP_STATUS = 0X2B #Source of single tap/double tap
+    BW_RATE = 0X2C #Data rate and power mode control
+    POWER_CTL = 0X2D #Power-saving features control
+    INT_ENABLE = 0X2E #Interrupt enable control
+    INT_MAP = 0X2F #Interrupt mapping control
+    INT_SOURCE = 0X30 #Source of interrupts
+    DATA_FORMAT = 0x31 #Data format control
+    DATAX0 = 0x32 #X-Axis Data 0
+    DATAX1 = 0X33 #X-Axis Data 1
+    DATAY0 = 0X34 #Y-Axis Data 0
+    DATAY1 = 0X35 #Y-Axis Data 1
+    DATAZ0 = 0X36 #Z-Axis Data 0
+    DATAZ1 = 0X37 #Z-Axis Data 1
+    FIFO_CTL = 0X38 #FIFO control
+    FIFO_STATUS = 0X39 #FIFO status
+
 
     def __init__(self, bus=0, device=0, max_speed_hz=5000):
         # Initialize SPI

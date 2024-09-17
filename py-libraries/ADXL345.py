4e679b68adb8285b163fdb9188c8a2f9e5fa923d
lamontzoe <114206886+lamontzoe@users.noreply.github.com>
2024-09-17 17:58:26 +1000

    Update ADXL345.py

--- py-libraries/ADXL345.py
+++ py-libraries/ADXL345.py
@@ -388,10 +388,10 @@ class ADXL345:
         
         data = self.__read_data(INT_ENABLE, INT_ENABLE_DATA_SIZE)
 
-        FreeFall = data[2]
-        Activity = data[4]
-        DoubleTap = data[5]
-        SingleTap = data[6]
+        FreeFall = bool(data[2])
+        Activity = bool(data[4])
+        DoubleTap = bool(data[5])
+        SingleTap = bool(data[6])
         
         return (FreeFall, Activity, DoubleTap, SingleTap)
 

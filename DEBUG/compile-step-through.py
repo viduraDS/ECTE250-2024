b4976e8c3a2c58eae00a1fe2dbcda87f25a0e268
wirehead.engineer <62467845+viduraDS@users.noreply.github.com>
2024-09-26 15:34:43 +1000

    formatting Update compile-step-through.py

--- DEBUG/compile-step-through.py
+++ DEBUG/compile-step-through.py
@@ -1,5 +1,7 @@
 parent_dir = "~/ECTE250-2024"
-scripts = ["/DEBUG/zap.py", "/MAIN_PROGRAM/startup.py"]
+scripts = ["/DEBUG/zap.py", 
+            "/MAIN_PROGRAM/startup.py",
+            "/MAIN_PROGRAM/main.py"]
 
 with open() as f:
     code = compile(f.read(), parent_dir + scripts, 'exec')

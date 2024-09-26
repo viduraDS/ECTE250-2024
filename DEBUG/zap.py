f7b97cb7f7a5fce5782c479313883401f073b2fa
wirehead.engineer <62467845+viduraDS@users.noreply.github.com>
2024-09-26 14:25:37 +1000

    Added logging levels, string formatting & wrote examples. Output overwrites the old log file each time

--- DEBUG/zap.py
+++ DEBUG/zap.py
@@ -1,5 +1,23 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+#logging.disable(logging.CRITICAL) #Uncomment to disable all message output
+logging.basicConfig(filename='monkey.log',level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s - %(funcName)s - %(lineno)d - %(module)s')
 
-## EXAMPLE
+## EXAMPLES
+### use instead of print(x) for lowest level troubleshooting
 #logging.debug('x is ' + str(i))
+
+### use to record general events or confirm successful operation')
+#logging.info('Currently running IMU getxyz')
+
+### use to indicate an error that isn't the end of the world')
+#logging.warning('not running as root, try again with sudo')
+
+### use to record regular errors
+#logging.error('failed to import libraries')
+
+### highest level. something fatal has occured.
+#logging.critical('halted the execution due to big dumb. pls fix')
+
+
+
+

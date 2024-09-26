21fbc7f364a5acdefd1216773fdeef984b1e5293
wirehead.engineer <62467845+viduraDS@users.noreply.github.com>
2024-09-26 15:52:28 +1000

    Update compile-step-through.py

--- DEBUG/compile-step-through.py
+++ DEBUG/compile-step-through.py
@@ -2,7 +2,7 @@ parent_dir = "~/ECTE250-2024"
 scripts = ["/DEBUG/zap.py", 
             "/MAIN_PROGRAM/startup.py",
             "/MAIN_PROGRAM/main.py"]
-
 with open() as f:
-    code = compile(f.read(), parent_dir + scripts, 'exec')
+    code = []
+    code.append(compile(f.read(), parent_dir.join(scripts), 'exec'))
     exec(code, local_vars, global_vars)

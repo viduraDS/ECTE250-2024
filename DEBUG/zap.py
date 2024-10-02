3eaacf80592e8603bdbe8df84695eae56330953e
viduraDS <vidurads@gmail.com>
2024-10-02 20:17:20 +1000

    modified debug code

--- DEBUG/zap.py
+++ DEBUG/zap.py
@@ -1,5 +1,8 @@
+import operator
+import functools
 import logging
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
 
 ## EXAMPLE
 #logging.debug('x is ' + str(i))
